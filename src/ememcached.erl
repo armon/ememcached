ff04455658f5f255ffe7d798e6c5babf956fe8bf
Armon Dadgar <armon.dadgar@gmail.com>
2011-03-07 19:31:43 -0800

    Adding a little to the higher level ememcached

--- src/ememcached.erl
+++ src/ememcached.erl
@@ -1,4 +1,12 @@
 -module(ememcached).
+-compile(export_all).
+-export([start/0]).
 
 % This is the main entry point for the system and
 % boostraps the rest of the system.
+
+start() ->
+  % Start the connection handler
+  conn_handler:start().
+
+
