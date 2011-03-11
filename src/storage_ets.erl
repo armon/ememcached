8dabfb1769ccd2fc8ed3c8ff44900571c681cb95
Armon Dadgar <armon.dadgar@gmail.com>
2011-03-10 17:43:42 -0800

    Moved creating the table to a process that will not die. Added set command.

--- src/storage_ets.erl
+++ src/storage_ets.erl
@@ -5,7 +5,19 @@
 
 % Initializes the table store
 start() ->
-    % Create our ETS table
-    ets:new(storage_ets_table, [set,public,named_table]).
+    % Create our ETS table, use a new thread so that the table will continue to exist.
+    spawn(fun() -> 
+        ets:new(storage_ets_table, [set,public,named_table, {write_concurrency,true}, {read_concurrency, true}]),
+        receive _ -> void end
+    end).
+
+% Takes an entry, returns a tuple we can use
+entry_to_tuple(Entry) -> {Entry#entry.key, Entry}.
+
+% Called with an entry to set, no pre/post conditions
+% @spec set(entry()) -> stored
+set(Entry) -> ets:insert(storage_ets_table, entry_to_tuple(Entry)), stored.
+
+
 
 
