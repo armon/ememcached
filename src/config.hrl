08d5b8159f337a065075ae57f58e51969560e94c
Armon Dadgar <armon.dadgar@gmail.com>
2011-03-10 17:44:04 -0800

    Added set interface to storage, moved the setting of the backend into config.hrl

---
+++ src/config.hrl
@@ -0,0 +1,8 @@
+% This file is used to define our configuration
+
+% The time we wait for the data block to come after a command is issued
+-define(ASCII_RECV_TIMEOUT_MILLI, 10000).
+
+% This is the storage backend that we use
+-define(STORAGE_BACKEND, storage_ets).
+
--- src/storage.erl
+++ src/storage.erl
@@ -1,12 +1,16 @@
 -module(storage).
 -compile(export_all).
--define(STORAGE_BACKEND, storage_ets).
 -include("entries.hrl").
+-include("config.hrl").
 
 % This module interfaces around the implementing backend,
 % which could be ETS, DETS, or anything else.
 
 % Starts the backend (if necessary)
 % @spec start() -> void().
-start() -> apply(?STORAGE_BACKEND, start, 0).
+start() -> apply(?STORAGE_BACKEND, start, []).
+
+% Sets an item in the backend
+% @spec set(entry()) -> stored
+set(Entry) -> io:format("Called set ~p~n", [Entry#entry.key]), apply(?STORAGE_BACKEND, set, [Entry]).
 
